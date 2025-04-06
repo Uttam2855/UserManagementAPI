057455cca4134d12761be815d02e57a0f66877d4
Uttam2855 <139743319+Uttam2855@users.noreply.github.com>
2025-04-06 17:51:50 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using UserManagementAPI.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseRequestLogging(); // Logging Middleware
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
